7bd0df47a26b6707d5494713951836a18f28cda5
ErwanDelory <erwan.delory@gmail.com>
2020-08-05 17:04:18 +0200

    add hook state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent} from 'react';
+import React, { FunctionComponent, useState} from 'react';
 
 //const App: React.FC
 const App: FunctionComponent = () => {
-  const name: String = 'React';
+  //const name: String = 'React';
+  const [name, setName] = useState('React');
 
   return (
     <h1>Hello, { name } !</h1>
