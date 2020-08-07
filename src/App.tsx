3daf0e8718630ebf7bb49dceb838530a02def16b
ErwanDelory <erwan.delory@gmail.com>
2020-08-07 17:07:42 +0200

    Changing indentation and data display

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,38 @@ import POKEMONS from './models/mock-pokemon';
 
 //const App: React.FC
 const App: FunctionComponent = () => {
-    //const name: String = 'React';
-    //const [name, setName] = useState<String>('React');
+  //const name: String = 'React';
+  //const [name, setName] = useState<String>('React');
 
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-    useEffect(() => {
-        setPokemons(POKEMONS);
-    }, []);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  useEffect(() => {
+    setPokemons(POKEMONS);
+  }, []);
 
-    return (
-        <div>
-            <h1>Pokédex</h1>
-            <p>Il y a { pokemons.length } pokémons dans le Pokédex</p>
+  return (
+    <div>
+      <h1 className="center">Pokédex</h1>
+      <div className="container">
+        <div className="row">
+          {pokemons.map(({ id, name, picture, created }) => (
+            <div className="col s6 m4" key={id}>
+              <div className="card horizontal">
+                <div className="card-image">
+                  <img src={picture} alt={name}></img>
+                </div>
+                <div className="card-stacked">
+                  <div className="card-content">
+                    <p>{name}</p>
+                    <p><small>{created.toString()}</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default App; 
