b6131a05e7597934decaf3cc07c5ea4126b00e89
ErwanDelory <erwan.delory@gmail.com>
2020-08-07 16:13:39 +0200

    add component pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React, { FunctionComponent, useState} from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
 
 //const App: React.FC
 const App: FunctionComponent = () => {
-  //const name: String = 'React';
-  const [name, setName] = useState('React');
+    //const name: String = 'React';
+    //const [name, setName] = useState<String>('React');
 
-  return (
-    <h1>Hello, { name } !</h1>
-  )
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    useEffect(() => {
+        setPokemons(POKEMONS);
+    }, []);
+
+    return (
+        <div>
+            <h1>Pokédex</h1>
+            <p>Il y a { pokemons.length } pokémons dans le Pokédex</p>
+        </div>
+    )
 }
 
 export default App; 
