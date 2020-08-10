2cf374b0e0f0464ba49a3bcef8eb2e14133f2b85
ErwanDelory <erwan.delory@gmail.com>
2020-08-10 16:58:41 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './pages/page-not-found';
 
 //const App: React.FC
 const App: FunctionComponent = () => {
@@ -7,7 +10,23 @@ const App: FunctionComponent = () => {
   //const [name, setName] = useState<String>('React');
 
   return (
-    <PokemonList />
+    <Router>
+      <div>
+        {/* La barre de navigation commune à toutes les pages */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">Pokédex</Link>
+          </div>
+        </nav>
+        {/* Le système de gestion des routes de notre application */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route exact path="/pokemons/:id" component={PokemonsDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
