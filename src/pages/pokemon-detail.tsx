ecda6e7537c0ab747b04cd013a6bfc2d79894175
ErwanDelory <erwan.delory@gmail.com>
2020-08-11 16:54:26 +0200

    add services

--- src/pages/pokemon-detail.tsx
+++ src/pages/pokemon-detail.tsx
@@ -1,10 +1,11 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import Pokemon from '../models/pokemon';
-import POKEMONS from '../models/mock-pokemon';
+//import POKEMONS from '../models/mock-pokemon';
 import formatDate from '../helpers/format-date';
 import formatType from '../helpers/format-type';
 import './pokemon-detail.css'
+import PokemonService from '../services/pokemon-service';
   
 type Params = { id: string };
   
@@ -13,11 +14,12 @@ const PokemonsDetail: FunctionComponent<RouteComponentProps<Params>> = ({ match 
   const [pokemon, setPokemon] = useState<Pokemon|null>(null);
   
   useEffect(() => {
-    POKEMONS.forEach(pokemon => {
+    /*POKEMONS.forEach(pokemon => {
       if (match.params.id === pokemon.id.toString()) {
         setPokemon(pokemon);
       }
-    })
+    })*/
+    PokemonService.getPokemon(+match.params.id).then(pokemon => setPokemon(pokemon));
   }, [match.params.id]);
     
   return (
