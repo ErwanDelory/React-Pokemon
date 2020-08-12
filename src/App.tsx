e3f424a13d6c3c57ff18bc61e81d3572ddc056fc
ErwanDelory <erwan.delory@gmail.com>
2020-08-12 16:51:52 +0200

    login page and routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PageNotFound from './pages/page-not-found';
 import PokemonEdit from './pages/pokemon-edit';
 import PokemonAdd from './pages/pokemon-add';
+import Login from './pages/login';
+import PrivateRoute from './PrivateRoute';
 
 //const App: React.FC
 const App: FunctionComponent = () => {
@@ -22,11 +24,12 @@ const App: FunctionComponent = () => {
         </nav>
         {/* Le système de gestion des routes de notre application */}
         <Switch>
-          <Route exact path="/" component={PokemonList} />
-          <Route exact path="/pokemons" component={PokemonList} />
-          <Route exact path="/pokemons/add" component={PokemonAdd} />
-          <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
-          <Route exact path="/pokemons/:id" component={PokemonsDetail} />
+          <PrivateRoute exact path="/" component={PokemonList} />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/pokemons" component={PokemonList} />
+          <PrivateRoute exact path="/pokemons/add" component={PokemonAdd} />
+          <PrivateRoute exact path="/pokemons/edit/:id" component={PokemonEdit} />
+          <PrivateRoute exact path="/pokemons/:id" component={PokemonsDetail} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
